bc3d8ae63d29c10f0cf3b47cab2810d9eaf83dbd
acscube360 <acscube360@gmail.com>
2023-04-05 07:52:23 +0530

    add files 2023/04/05

--- app/src/main/java/com/example/pharmacare/AddItemActivity.java
+++ app/src/main/java/com/example/pharmacare/AddItemActivity.java
@@ -3,12 +3,27 @@ package com.example.pharmacare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class AddItemActivity extends AppCompatActivity {
+import com.example.pharmacare.ui.PopupClass;
+
+public class AddItemActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+        findViewById(R.id.iv_search).setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.iv_search:
+                PopupClass popupClass=new PopupClass();
+                popupClass.showPopupWindow(v);
+        }
     }
 }
